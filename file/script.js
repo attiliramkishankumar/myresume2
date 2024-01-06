b9a5e12450ae6c66a3df37b0d21c178bd2d4f3f1
attiliramkishankumar <155656335+attiliramkishankumar@users.noreply.github.com>
2024-01-06 11:41:45 +0530

    Add files via upload

---
+++ file/script.js
@@ -0,0 +1,11 @@
+// script.js
+// Function to open email 
+function contactMe() {
+    window.location.href = 'cont.html';
+  }
+  
+  // Function to download of the resume file
+  function downloadResume() {
+    window.location.href = 'files.docx';
+  }
+  
